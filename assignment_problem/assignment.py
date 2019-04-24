462bc9e666c340df4ece5f98da02d54874047468
juliancoffee <lightdarkdaughter@gmail.com>
2019-04-25 00:01:08 +0300

    assignment problem

---
+++ assignment_problem/assignment.py
@@ -0,0 +1,26 @@
+import itertools
+import random
+import time
+import sys
+
+setup = time.time()
+
+n = int(sys.argv[1])
+
+work_matrix = [[random.randint(1, 11) for i in range(n)] for j in range(n)]
+for i in range(n):
+    print(i, work_matrix[i])
+print("\n")
+
+perms = list(itertools.permutations(range(n)))
+
+res = float("inf")
+for perm in perms:
+    plan = []
+    for task in range(n):
+            plan.append(work_matrix[perm[task]][task])
+    if sum(plan) < res:
+        res = sum(plan)
+        print("For {} => {}".format(perm, plan))
+print("Result = {}".format(res))
+print("Time = {}".format(time.time() - setup))
